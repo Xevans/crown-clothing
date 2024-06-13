cd6413684bb1ec3afe79e170de9f873e60cd7196
xavier evans <evans.xavier@ymail.com>
2024-06-13 12:14:59 -0400

    Removed Shop style file

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -3,7 +3,6 @@ import { Routes, Route } from 'react-router-dom';
 import Category from '../category/category.component';
 import CategoriesPreview from '../categories-preview/categories-preview.component';
 
-import './shop.styles.scss';
 
 const Shop = () => {
 
