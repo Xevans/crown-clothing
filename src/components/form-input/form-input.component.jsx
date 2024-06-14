0dcc8e6d5104f240dd1572f52434a2ffa1b4128a
Xevans <43121988+Xevans@users.noreply.github.com>
2024-06-14 09:06:05 -0400

    converted form input component to styled com.

--- src/components/form-input/form-input.component.jsx
+++ src/components/form-input/form-input.component.jsx
@@ -1,18 +1,18 @@
-import "./form-input.styles.scss"
+import {FormInputLabel, Input, Group} from "./form-input.styles.jsx"
 
 const FormInput = ({ label, inputOptions }) => { // deconstucted from props
 
     return(
-        <div className="group">
-            <input className="form-input" {...inputOptions} /> {/* other props will apply all other props that we would otherwise hard code here: <input type='text' required onChange={handleChange} name='username' value={username} /> */}
+        <Group>
+            <Input {...inputOptions} /> {/* other props will apply all other props that we would otherwise hard code here: <input type='text' required onChange={handleChange} name='username' value={username} /> */}
             {/* If no label prop is passed into this component, dont render the label. So check if it exists first */}
             {label && (
-                <label className={`${inputOptions.value.length ? 'shrink' : ''} form-input-label`}> {/* if the length of the string in value > 0/ if the string in value exists, apply the shrink class, otherwise, apply the class '' (<- none basically). form-input-label will still apply regardless since it is not declared as conditional*/}
+                <FormInputLabel shrink={inputOptions.value.length}> {/* if the length of the string in value > 0/ if the string in value exists, apply the shrink class, otherwise, apply the class '' (<- none basically). form-input-label will still apply regardless since it is not declared as conditional*/}
                     {label}
-                </label>
+                </FormInputLabel>
             )}
-        </div>
-    )
-}
+        </Group>
+    );
+};
 
 export default FormInput;
